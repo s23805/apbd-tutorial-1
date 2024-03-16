191edd4049058670f909b76f9ce1d2c20df72e33
s23805 <s23805@pjwstk.edu.pl>
2024-03-17 00:38:13 +0100

    CalculateAverage method method

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Average {
 
-Console.WriteLine("THis is 1 modification");
+    public static double CalculateAverage(int[] numbers) {
+        
+        if(numbers == null || numbers.Length == 0)
+            throw new ArgumentException("Array is empty. Provide array with numbers");
 
-Console.WriteLine("modification 2");
+        int sum = 0;
+        foreach(int num in numbers){
+            sum += num;
+        }    
+        
+        return sum/numbers.Length;
+    }
 
-Console.WriteLine("Modification 3.");
+}
+
+class Program {
+    static void Main(string[] args)
+    {
+        int[] numbers = { 1, 2, 3, 4, 5};
+       
+        double average = Average.CalculateAverage(numbers);
+        
+        Console.WriteLine($"The average is: {average}");
+    }
+}
