856e78948b242d6a66b25a72f5d46c7ea551f1d3
s23805 <s23805@pjwstk.edu.pl>
2024-03-17 00:47:57 +0100

    FindMax method Implemented

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,6 +13,20 @@
         return sum/numbers.Length;
     }
 
+    public static int FindMax(int[] numbers) {
+
+        if(numbers == null || numbers.Length == 0)
+            throw new ArgumentException("Array is empty. Provide array with numbers");
+
+        int max = numbers[0];
+        foreach(int num in numbers){
+            if(num > max)
+                max = num;
+        }
+
+        return max;
+    }
+
 }
 
 class Program {
@@ -21,7 +35,9 @@ class Program {
         int[] numbers = { 1, 2, 3, 4, 5};
        
         double average = Average.CalculateAverage(numbers);
-        
+        int max = Average.FindMax(numbers);
+
         Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The max number is: {max}");
     }
 }
